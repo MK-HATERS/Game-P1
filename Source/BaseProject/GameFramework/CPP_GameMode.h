bc9be12580483bc8e0daa759dcc1b04b5ed249ac
MKHATERS <jpearson96785@gmail.com>
2024-06-21 15:36:35 -0600

    added simple cpp base class struct

---
+++ Source/BaseProject/GameFramework/CPP_GameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "CPP_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEPROJECT_API ACPP_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
